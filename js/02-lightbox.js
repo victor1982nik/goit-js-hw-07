9682531666c85c5d63e1f5be7b1006b2c249b889
Victor Nikolenko <victor1982nik@gmail.com>
2022-08-25 21:09:39 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+//galleryRef.addEventListener('click', onPictureClick);
+
+render(galleryItems);
+
+function render(gallery) {
+    const arr = gallery.map(({original, preview, description }) => {                 
+        return  `<li><a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a></li>`;        
+    }).join('');
+    
+    galleryRef.insertAdjacentHTML('beforeend', arr); 
+ }
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+ 
+ /*
+function onPictureClick(e) {
+    e.preventDefault();
+    
+    if (e.target.classList.value !== "gallery__image")         
+        return;   
+    e.stopPropagation();
+}*/
+
+
+
